e66ff28d4088002971517f8ecd74e91f8d0fac29
2529716798 <2529716798@qq.com>
2018-07-25 17:05:23 +0800

    finish calculator

--- Calculator/Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/Calculator/ViewController.swift
@@ -9,7 +9,96 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var firstOperand = 0.0
+    var secondOperand = 0.0
+    var isDecimal = false
+    var oprd = ""
+    var isSecond = false
+    @IBOutlet weak var resultLabel: UILabel!
+    
 
+    @IBAction func buttonTap(_ sender: UIButton) {
+        if resultLabel.text == "0" || (isSecond && secondOperand == 0.0){
+                resultLabel.text = ""
+        }
+        // the number occur in label
+        resultLabel.text = resultLabel.text! + sender.titleLabel!.text!
+        
+        if isSecond{
+            secondOperand = (resultLabel.text! as NSString).doubleValue
+        }else{
+            firstOperand = (resultLabel.text! as NSString).doubleValue
+        }
+        // print(firstOperand)
+        
+    }
+    
+    @IBAction func DecimalPointTap() {
+        if !isDecimal{
+            resultLabel.text = resultLabel.text! + "."
+            if isSecond{
+                secondOperand = NSString(string: resultLabel.text!).doubleValue
+            }else{
+                firstOperand = NSString(string: resultLabel.text!).doubleValue
+            }
+            isDecimal = !isDecimal
+        }
+    
+    }
+    
+    
+    @IBAction func OperatorTap(_ sender: UIButton) {
+        if firstOperand != 0{
+            isSecond = true
+            isDecimal = false
+            
+            let text = sender.titleLabel!.text!
+            if text == "+" || text == "-" || text == "*" || text == "÷"{
+                oprd = text
+                
+            }else{
+                oprd = ""
+            }
+        }
+    }
+    
+    @IBAction func resultTap() {
+        if isSecond{
+            if oprd == "÷" && secondOperand == 0{
+                print("Divisor can't be 0...")
+                return
+            }
+            var result: Double = 0.0
+            switch oprd {
+            case "+":
+                result = firstOperand + secondOperand
+            case "-":
+                result = firstOperand - secondOperand
+            case "*":
+                result = firstOperand * secondOperand
+            case "÷":
+                result = firstOperand / secondOperand
+            default:
+                result = 0.0
+            }
+            resultLabel.text = result.description
+            print("First operand: \(  firstOperand)")
+            print("Second operand:  \(secondOperand)")
+            print("Operator: \(oprd)")
+            print("result: \(result)")
+        }
+    }
+    
+    @IBAction func clear() {
+        resultLabel.text = "0"
+        firstOperand = 0.0
+        secondOperand = 0.0
+        isDecimal = false
+        isSecond = false
+        oprd = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
